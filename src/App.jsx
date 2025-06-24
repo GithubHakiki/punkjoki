cbe5b42baa77f2b9c27f56c26076b8a6e36ad678
GithubHakiki <hakikimuhammad@students.amikom.ac.id>
2025-06-24 17:44:46 +0700

    GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Analytics } from "@vercel/analytics/react";
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import useDocumentSetup from './components/document/documentSetup.jsx';
 
 import './App.css';
@@ -12,9 +16,21 @@ import PartnersSection from './components/Partner.jsx';
 import TestimonialSection from './components/Testimonial.jsx';
 import Footer from './components/Footer.jsx';
 
-const App = () => {
+// ✅ Inisialisasi GA sekali aja saat app load
+ReactGA.initialize('G-NK4KQBESKL');
 
+// ✅ Custom hook untuk tracking pageview
+const usePageView = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
+};
+
+const App = () => {
   useDocumentSetup();
+  usePageView(); // aktifkan tracking
 
   return (
     <div className="App">
